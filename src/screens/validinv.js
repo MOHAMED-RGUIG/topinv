96dd72112ed1f5974e4b21e0b4aad0e085ab06af
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-12-13 12:07:26 +0100

    Update validinv.js

--- src/screens/validinv.js
+++ src/screens/validinv.js
@@ -199,7 +199,7 @@ const handleInputCodeChange = (e) => {
            <input
         required
         type="text"
-        placeholder="Scan QR Code ou Code-barres"
+        placeholder="Code-barre"
         className="form-control col-xl-10 col-8 col-md-8 mx-auto"
         value={EANCOD_0}
         onChange={handleInputCodeChange}  // Met à jour le résultat du scan dans l'input
