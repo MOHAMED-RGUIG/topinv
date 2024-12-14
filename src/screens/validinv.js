a735e1588a7167a087505921a7f2c33e2b41a006
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-12-14 13:04:12 +0100

    Update validinv.js

--- src/screens/validinv.js
+++ src/screens/validinv.js
@@ -319,7 +319,7 @@ const handleInputCodeChange = (e) => {
                 placeholder="Résultat QR Code"
                 className="form-control col-xl-10 col-8 col-md-8 mx-auto"
                 value={scanResult}
-                onChange={(e) => handleInputCodeChange(e.target.value)}
+                onChange={(e) => handleInputCodeChange(scanResult)}
                 style={{ width: "90%", fontSize: "13px", marginTop: "10px" }}
             />
 
