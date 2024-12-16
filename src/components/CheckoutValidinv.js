043c9cd54f3087f371f9ad5b3559363f218d55cc
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-12-16 13:39:50 +0100

    Update CheckoutValidinv.js

--- src/components/CheckoutValidinv.js
+++ src/components/CheckoutValidinv.js
@@ -8,7 +8,7 @@ import Error from '../components/Error';
 import Success from '../components/Success';
 import emailjs from 'emailjs-com';
 
-function CheckoutValidinv({ REFINV_0, ITMREF_0, localData }) {
+function CheckoutValidinv({ REFINV_0, itmref, localData }) {
     const validinvinsertstate = useSelector(state => state.validInvReducer);
     const { loading, error, success } = validinvinsertstate;
     const currentUser = JSON.parse(localStorage.getItem('currentUser'));
@@ -16,7 +16,7 @@ function CheckoutValidinv({ REFINV_0, ITMREF_0, localData }) {
     const dispatch = useDispatch();
 
     function tokenHandler() {
-        if (REFINV_0 && ITMREF_0 && localData.length > 0) {
+        if (REFINV_0 && itmref && localData.length > 0) {
             const payload = {
                 REFINV_0,
                 ITMREF_0,
