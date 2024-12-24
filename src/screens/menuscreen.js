a4fae3a2ef30d1d8af644cd9e05070cf59a86eab
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-12-24 16:34:35 +0100

    Update menuscreen.js

--- src/screens/menuscreen.js
+++ src/screens/menuscreen.js
@@ -1,18 +1,18 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 function menuscreen() {
   return (
-    <div className='col-9 col-md-9 col-xs-9 mx-auto mt-3 pt-3' >  
-        <a   href="/homescreen">        <button className='btn5 mt-2 p-4'  >LISTE INVENTAIRE</button>
-
-            </a>   
-            <a  href="/cart">    
-         <button className='btn5 mt-2 p-4'  href="/cart" >CREATION INVENTAIRE</button>
-         </a>
-         <a  href="/validinv">    
-         <button className='btn5 mt-2 p-4'  href="/cart" >VALIDATION INVENTAIRE</button>
-         </a>
-         </div>
+    <div className='col-9 col-md-9 col-xs-9 mx-auto mt-3 pt-3'>
+      <Link to="/homescreen">
+        <button className='btn5 mt-2 p-4'>LISTE INVENTAIRE</button>
+      </Link>
+      <Link to="/cart">
+        <button className='btn5 mt-2 p-4'>CREATION INVENTAIRE</button>
+      </Link>
+      <Link to="/validinv">
+        <button className='btn5 mt-2 p-4'>VALIDATION INVENTAIRE</button>
+      </Link>
+    </div>
   )
 }
 
